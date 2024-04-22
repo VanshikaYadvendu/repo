bc9fb2a7fc99e9fcc34559f67ace7b178d32a3e2
VanshikaYadvendu <vanshikayadvendu@gmail.com>
2024-04-22 12:06:29 +0530

    first

---
+++ insertionsort.cpp
@@ -0,0 +1,39 @@
+//insertion sort
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+void insertionSort(int arry[],int size){
+    int i,j,key;
+    for(i=1;i<size;i++){
+        key=arry[i];
+        j=i-1;
+        while(j>=0 && arry[j]>key){
+            arry[j+1]=arry[j];
+            j--;
+        }
+        arry[j+1]=key;
+    }
+}//end of insertion sort
+
+//printing array
+void display(int a[],int n){
+  
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<"  ";
+    }
+}
+//drivers code 
+int main(){
+    int a[10];
+    for(int i=0;i<10;i++){
+        a[i]=rand()%20;
+    }
+    //printing before sorting
+    display(a,10);
+    
+    //applying sorting
+    insertionSort(a,10);
+    cout<<endl<<"after sorting:"<<endl;
+    display(a,10);
+}
